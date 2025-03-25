05bb0bb4b77ea8e1645b5d3fbcde8f4a9b89eb80
yjsong2154 <yjsong2153@gmail.com>
2025-03-25 14:07:36 +0900

    Feat : 빈칸 자동 생성 구현

--- Assets/Scrips/UI/UIInventory.cs
+++ Assets/Scrips/UI/UIInventory.cs
@@ -4,10 +4,17 @@ using UnityEngine.UI;
 public class UIInventory : MonoBehaviour
 {
     [SerializeField] private Button backButton;
+    [SerializeField] private RectTransform content;
+    [SerializeField] private GameObject EmptySlot;
 
     void Start()
     {
         backButton.onClick.AddListener(OnBackButtonClicked);
+
+        for (int i = 0; i < 120; i++)
+        {
+            GameObject NoneSlot = Instantiate(EmptySlot, content);
+        }
     }
 
     private void OnBackButtonClicked()
