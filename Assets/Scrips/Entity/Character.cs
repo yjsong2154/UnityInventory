686868121438262609f6aeff08231128b71c3a25
yjsong2154 <yjsong2153@gmail.com>
2025-03-25 14:58:45 +0900

    Fix : item prefab에 버튼 기능 추가

--- Assets/Scrips/Entity/Character.cs
+++ Assets/Scrips/Entity/Character.cs
@@ -3,6 +3,7 @@ using UnityEngine.Timeline;
 
 public class Character
 {
+    public Item equipItem;
     public int Attack;
     public int Defence;
     public int Health;
@@ -14,5 +15,37 @@ public class Character
         Defence = defence;
         Health = health;
         Critical = critical;
+    }
+
+    public void Equip(Item item)
+    {
+        Debug.Log("장비");
+        if (equipItem == item)
+        {
+            unEquip();
+            Debug.Log($"해제 {Attack}, {Defence}, {Health}, {Critical}");
+        }
+        else
+        {
+            unEquip();
+            Attack += item.Attack;
+            Defence += item.Defence;
+            Health += item.Health;
+            Critical += item.Critical;
+            equipItem = item;
+            Debug.Log($"착용 {Attack}, {Defence}, {Health}, {Critical}");
+        }
+    }
+
+    private void unEquip()
+    {
+        if (equipItem != null)
+        {
+            Attack -= equipItem.Attack;
+            Defence -= equipItem.Defence;
+            Health -= equipItem.Health;
+            Critical -= equipItem.Critical;
+            equipItem = null;
+        }
     }
 }
--- Assets/Scrips/UI/UISlot.cs
+++ Assets/Scrips/UI/UISlot.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UISlot : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Button button;
+
+    private Item itemDate;
+
     void Start()
     {
-        
+        button.onClick.AddListener(OnItemButtonClick);
+        SetItem();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetItem()
     {
-        
+        itemDate = GetComponent<Item>();
+    }
+
+    public void OnItemButtonClick()
+    {
+        GameManager.Instance.Player.Equip(itemDate);
     }
 }
