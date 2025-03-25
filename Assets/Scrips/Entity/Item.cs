001f8f783176e870d4ba457c67703cdef0f3d78c
yjsong2154 <yjsong2153@gmail.com>
2025-03-25 14:27:13 +0900

    Fix : item prefab

---
+++ Assets/Scrips/Entity/Item.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public String Name;
+    public int Attack;
+    public int Defence;
+    public int Health;
+    public int Critical;
+}
